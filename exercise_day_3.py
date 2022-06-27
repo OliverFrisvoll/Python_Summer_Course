f390dd555355d330350523c0a643cd297843c455
Oliver Frisvoll <ocfris97@gmail.com>
2022-06-27 14:43:20 +0200

    Added exercise_day_3.py

--- exercise_day_3.py
+++ exercise_day_3.py
@@ -2,36 +2,54 @@
 import logging
 
 
-exchange_rates = {
-    "NOK/EUR": 1 / 10.40,
-    "NOK/USD": 1 / 9.91,
-}
+def currency_swap_1():
+    NOK_EUR = 10.40
+    amount = int(input("Amount of money to exchange (in NOK): "))
+    print(f"{amount} NOK equals {amount / NOK_EUR} EUR")
 
 
-def currency_swap():
-    des: int = 2
+def currency_swap_2_3():
+    NOK_EUR = 10.40
+    text = input("Amount of money to exchange with EUR or NOK at the end: ").upper()
+    amount = int(text.split(" ")[0])
+    if text.endswith("NOK"):
+        print(f"{amount} NOK equals {amount / NOK_EUR:.2f} EUR")
+    elif text.endswith("EUR"):
+        print(f"{amount} EUR equals {amount * NOK_EUR:.2f} NOK")
+    else:
+        print(" --- We currently do not support this currency, please try NOK or EUR --- ")
+
+
+def currency_swap_4():
+    exchange_rates = {
+        "NOK/EUR": 1 / 10.40,
+        "NOK/USD": 1 / 9.91,
+    }
+
     temp_dict = {}
     for key in exchange_rates:
         currencies = key.split("/")
         currencies.reverse()
-        temp_dict["/".join(currencies)] = round(1 / exchange_rates[key], des)
+        temp_dict["/".join(currencies)] = 1 / exchange_rates[key]
 
     exchange_rates.update(temp_dict)
     rate: str = ""
     while rate not in list(exchange_rates):
-        rate = input(f"Available rates: {' '.join(list(exchange_rates))}\nWhich exchange rate? ")
+        rate = input(f"Available rates: {' '.join(list(exchange_rates))}\nWhich exchange rate? ").upper()
         if rate not in list(exchange_rates):
             print(f" --- We currently do not support {rate}, please try a rate from the list --- ")
 
-    amount = round(float(input(f"Amount of money to exchange: ")), des)
-    print(f"{amount} {rate.split('/')[0]} equals {round(exchange_rates[rate] * amount, des)} {rate.split('/')[1]}")
+    amount = float(input(f"Amount of money to exchange: "))
+    print(f"{amount} {rate.split('/')[0]} equals {exchange_rates[rate] * amount:.2f} {rate.split('/')[1]}")
 
     if input("Try another? (Y/N): ") == "Y":
-        currency_swap()
+        currency_swap_4()
 
 
 def main():
-    currency_swap()
+    # currency_swap_1()
+    currency_swap_2_3()
+    # currency_swap_4()
 
 
 if __name__ == '__main__':
